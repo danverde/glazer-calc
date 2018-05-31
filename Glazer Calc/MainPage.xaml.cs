e39398cf677d26b1facabd19c5f2b0e0250a6ca9
danverde <gre12014@byui.edu>
2018-05-31 08:43:28 -0600

    Updated certificate location

--- Glazer Calc/MainPage.xaml.cs
+++ Glazer Calc/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace Clazer_Calc
         {
             this.InitializeComponent();
         }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
